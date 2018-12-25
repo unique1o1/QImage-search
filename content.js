ff7a24e34d0d5a55d78d9a383771a4d459793df8
Yunik Maharjan <yunik.maharjan@icloud.com>
2018-12-25 19:16:56 +0545

    create post request to file.io

--- content.js
+++ content.js
@@ -1,3 +1,27 @@
+function reverseImageSearch(rawimage) {
+  var blob = dataURItoBlob(rawimage);
+  var fd = new FormData();
+  fd.append("file", blob);
+
+  var xhr = new XMLHttpRequest();
+  xhr.responseType = "json";
+  xhr.open("POST", "https://file.io", true);
+  xhr.onload = function() {
+    // Request finished, now opening new tab with google image search url.
+    if (this.response.success && this.response.success === true) {
+      /*opening new tab with the search results*/
+      var searchURL =
+        "https://www.google.com/searchbyimage?&image_url=" + this.response.link;
+      chrome.tabs.create({
+        url: searchURL
+      });
+    } else {
+      console.log("Sorry, Unable to perform reverse search!");
+    }
+  };
+  xhr.send(fd);
+}
+
 function reverseSearch() {
   chrome.tabs.captureVisibleTab(function(screenshotUrl) {
     /*uploading the screenshot to a sever & generating url*/
@@ -6,12 +30,22 @@ function reverseSearch() {
     if (confirm("Do you want to crop the image?")) {
       // get cropped image & proceed
       getCroppedImage(screenshotUrl, "reversesearch");
+      chrome.runtime.onMessage.addListener(function(
+        message,
+        sender,
+        sendResponse
+      ) {
+        if (message.callbackMethod === "reversesearch") {
+          reverseImageSearch(message.croppedImage);
+        }
+        //removing message listener
+        chrome.runtime.onMessage.removeListener(arguments.callee);
+      });
     } else {
       reverseImageSearch(screenshotUrl);
     }
   });
 }
-
 function getCroppedImage(image, callbackMethod) {
   //   console.log("cropping image : callbackMethod : " + callbackMethod);
   chrome.tabs.query(
