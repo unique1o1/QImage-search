55a672771f1441612b1e2b37e74af974e0a382f6
Yunik Maharjan <yunik.maharjan@icloud.com>
2018-12-25 19:16:08 +0545

    inject cropperjs and content script to current tab

--- content.js
+++ content.js
@@ -4,8 +4,56 @@ function reverseSearch() {
 
     //asking for image crop from user
     if (confirm("Do you want to crop the image?")) {
+      // get cropped image & proceed
+      getCroppedImage(screenshotUrl, "reversesearch");
     } else {
       reverseImageSearch(screenshotUrl);
     }
   });
+}
+
+function getCroppedImage(image, callbackMethod) {
+  //   console.log("cropping image : callbackMethod : " + callbackMethod);
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true
+    },
+    function(tabs) {
+      var tabid = tabs[0].id;
+
+      chrome.tabs.executeScript(
+        tabid,
+        {
+          code:
+            'var imageurl ="' +
+            image +
+            '", callbackMethod = "' +
+            callbackMethod +
+            '";'
+        },
+        function() {
+          /*injecting cropperjs into current tab*/
+          chrome.tabs.executeScript(
+            tabid,
+            {
+              file: "./cropperjs/cropper.js"
+            },
+            function(response) {
+              /*injecting our content script into current tab*/
+              chrome.tabs.executeScript(
+                tabid,
+                {
+                  file: "./content_script.js"
+                },
+                function(response) {
+                  //   console.log("Indside background script!! id:" + tabid + ", response: " + JSON.stringify(response, null, 4));
+                }
+              );
+            }
+          );
+        }
+      );
+    }
+  );
 }
